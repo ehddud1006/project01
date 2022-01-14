85f6ecf8fe67d211251755a6afb9fea29b9cbc05
ehddud1006 <62373865+ehddud1006@users.noreply.github.com>
2022-01-14 12:33:47 +0900

    initial commit

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,21 +1,31 @@
 import React from "react";
 import Titles from "./Titles.module.scss";
+import Slider from "./Slider";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./blog.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Title = () => {
   return (
-    <div className={Titles.container}>
-      <div className={Titles.container__content}>
-        <h1 className={Titles.container__content__title}>Title of a longer featured blog post</h1>  
-        <p className={Titles.container__content__body}>
-          Multiple lines of text that form the lede, informing new readers
-          quickly and efficiently about what’s most interesting in this post’s
-          contents.
-        </p>
-        <p>
-          <a href="/#">
-            Continue reading...
-          </a>
-        </p>
+    <div>
+      <div className={Titles.container}>
+        <div className={Titles.container__content}>
+          <h1 className={Titles.container__content__title}>
+            Title of a longer featured blog post
+          </h1>
+          <p className={Titles.container__content__body}>
+            Multiple lines of text that form the lede, informing new readers
+            quickly and efficiently about what’s most interesting in this post’s
+            contents.
+          </p>
+          <p>
+            <a href="/#">Continue reading...</a>
+          </p>
+        </div>
+      </div>
+      <div className="container">
+        <Slider></Slider>
       </div>
     </div>
   );
